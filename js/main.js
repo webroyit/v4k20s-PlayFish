4c440f4e69a8e3dd74f17e69a5e142e4cf13a6b7
royweb <xytank@gmail.com>
2019-10-09 13:45:38 -0400

    Add a function to show the mobile nav links

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-console.log("It works");
+$(function() {
+	menu = $('nav ul');
+
+    $('#openup').on('click', function(e) {
+        e.preventDefault(); menu.slideToggle();
+    });
+  
+    $(window).resize(function(){
+        var w = $(this).width(); if(w > 550 && menu.is(':hidden')) {
+            menu.removeAttr('style');
+        }
+    });
+  
+    $('nav li').on('click', function(e) {                
+        var w = $(window).width(); if(w < 550 ) {
+            menu.slideToggle(); 
+        }
+    });
+
+    $('.open-menu').height($(window).height());
+});
