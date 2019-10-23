8983e76850544dd1ff38b35a7e48e7a746029dcb
royweb <xytank@gmail.com>
2019-10-23 11:16:45 -0400

    Add smooth scrolling

--- js/main.js
+++ js/main.js
@@ -19,3 +19,25 @@ $(function() {
 
     $('.open-menu').height($(window).height());
 });
+
+// For smooth scrolling
+// add onClicked on all "a" tag
+$('.cf a').on('click', function(event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        // add animation
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top
+            },
+            // speed of the scoll
+            600,
+            function() {
+                window.location.hash = hash;
+            }
+        );
+    }
+});
