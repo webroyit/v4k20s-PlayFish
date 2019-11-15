c34b3f0d1cd43eeaaa11d79ec5e6ab61c3adeb3f
royweb <xytank@gmail.com>
2019-11-15 12:45:58 -0500

    Add a function called init

--- js/typeWriter.js
+++ js/typeWriter.js
@@ -2,11 +2,22 @@ const TypeWrite = function(txtElement, words, wait = 3000){
     this.txtElement = txtElement;
     this.words = words;
     this.txt = "";
-
-    // first word in the array
-    this.word = 0;
-
+    this.word = 0;  // first word in the array
     this.wait = parseInt(wait, 10);
     this.type();
     this.isDeleting = false;
+}
+
+// excute the function when the page load
+document.addEventListener('DOMContentLoaded', init);
+
+// init
+function init(){
+    // access the html element and attribute
+    const txtElement = document.querySelector('.txt-type');
+    const words = JSON.parse(txtElement.getAttribute('data-words'));    // JSON.parse convert the string into array
+    const wait = txtElement.getAttribute('data-wait');
+
+    // init typewriter
+    new TypeWrite(txtElement, words, wait);
 }
